eeb14f1c0431077a9f6a0f6ddf9cf168c2303317
slawek <slawek.piela@koios-mail.pl>
2024-01-07 23:57:32 +0100

    introducing sessions

--- web_chat.py
+++ web_chat.py
@@ -12,13 +12,23 @@ def initialize_session_data():
 # Initialize session data
 initialize_session_data()
 
+col1, col2 = st.columns([3, 1])
+with col1:
+    st.write("Response:", "")
+    #st.write("Full Response:", "")
+    st.write("Thread Trace:", "")
+with col2:
+    choice1 = st.radio("Wybierz model AI: ", ['GPT', 'Anton'])
 
-st.write(f"Session Counter: {st.session_state['user_data']['counter']}")
+
+
 
 # Example of handling other session-specific data
-user_input = st.text_input("Enter some data:")
+prompt = st.text_input("Prompt:")
 if st.button('Update Data'):
-    st.session_state['user_data']['prompt'] = user_input
+    st.session_state['user_data']['prompt'] = prompt
+
+
 
 if st.session_state['user_data']['prompt']:
     st.write(f"You entered: {st.session_state['user_data']['prompt']}")
