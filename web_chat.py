913c2af58832b12619b3a25cb128b60f9113e128
slawek <slawek.piela@koios-mail.pl>
2024-01-07 22:47:59 +0100

    updated for work with streamlit v14

--- web_chat.py
+++ web_chat.py
@@ -5,7 +5,7 @@ import os
 assistent = str(os.getenv('assistant_id4'))
 apik = str(os.getenv('api_key'))
 apik_t = type(apik)
-assist_t = type(assist)
+assist_t = type(assistent)
 assist_pr = assistent + " " +assist_t
 apik_pr = apik + " "+  apik_t
 
