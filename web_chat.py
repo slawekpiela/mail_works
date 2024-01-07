222482550463f7bcae3d01841fdc5e2a45038f58
slawek <slawek.piela@koios-mail.pl>
2024-01-08 00:24:45 +0100

    introducing sessions

--- web_chat.py
+++ web_chat.py
@@ -3,13 +3,16 @@ from query_openai import query_model
 import os
 import uuid
 
+
 # Function to initialize session-specific data
 def initialize_session_data():
     if 'user_data' not in st.session_state:
         # Initialize data specific to this session
         st.session_state['user_data'] = {
-            'counter': 0,
-            'prompt': None
+            'prompt': None,
+            'instructions': None,
+            'thread': None,
+            'assistant': None
         }
 
 
@@ -17,14 +20,12 @@ def initialize_session_data():
 
 initialize_session_data()
 
-
 assistant = str(os.getenv('assistant_id4'))
 
 # draw the page
 st.title("KOIOS v0.1")
 if 'session_id' not in st.session_state:
     st.session_state['session_id'] = str(uuid.uuid4())
-
 
 col1, col2 = st.columns([3, 1])
 with col1:
@@ -36,16 +37,23 @@ with col2:
 
 # Example of handling other session-specific data
 prompt = st.text_input("Prompt:")
+instructions = 'You are an assistant'
+if st.session_state['user_data']['thread']=='':
+    st.session_state['user_data']['thread']=''
+
 if st.button('Send'):
     st.session_state['user_data']['prompt'] = prompt
+    st.session_state['user_data']['instructions'] = instructions
+    st.session_state['user_data']['assistant'] = assistant = str(os.getenv('assistant_id4'))
+
 
 if st.session_state['user_data']['prompt']:
-    instructions = "you chat with me. if you find nothing in the files, search internet"
+    response_ai, full_response, thread_from_query = query_model(prompt, instructions, assistant,st.session_state['user_data']['thread'])
+    st.session_state['user_data']['thread'] = thread_from_query
 
-    response_ai, full_response, thread = query_model(prompt, instructions, assistant)
     with col1:
         st.write("Response:", response_ai)
         # st.write("Full Response:", full_response)
-        st.write("Thread Trace:", thread.id)
+        st.write("Thread Trace:", st.session_state['user_data']['thread'])
 
     st.write(f"You entered: {st.session_state['user_data']['prompt']}")
