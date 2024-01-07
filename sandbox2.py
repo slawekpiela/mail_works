dc4a7a347068aa4f6af83f9ef69e392031a6e691
slawek <slawek.piela@koios-mail.pl>
2024-01-07 20:11:38 +0100

    updated for work with streamlit

--- sandbox2.py
+++ sandbox2.py
@@ -1,11 +1,16 @@
-def remove_bracketed_part(timestamp_str):
-    # Split the string on space and take all parts except the last one (in brackets)
-    parts_without_brackets = timestamp_str.split()[:-1]
-    # Join these parts back into a string
-    adjusted_timestamp = ' '.join(timestamp_str.split()[:-1])
-    return adjusted_timestamp
+import streamlit as st
 
-# Example usage
-timestamp = "Fri, 29 Dec 2023 10:42:19 -0800 (PST)"
-adjusted_timestamp = remove_bracketed_part(timestamp)
-print(adjusted_timestamp)
+# Initialize session state for storing appended text if it doesn't already exist
+if 'appended_text' not in st.session_state:
+    st.session_state['appended_text'] = ''
+
+# Text input for new text
+new_text = st.text_input("Enter text to append:")
+
+# Button to append the text
+if st.button("Append Text"):
+    # Append new text to the existing text
+    st.session_state['appended_text'] += new_text + '\n'  # Adding a newline for separation
+
+# Display the appended text with red color using Markdown and HTML
+st.markdown(f"<p style='color: red;'>{st.session_state['appended_text']}</p>", unsafe_allow_html=True)
--- web_chat.py
+++ web_chat.py
@@ -3,10 +3,19 @@ import openai
 from query_openai import query_model
 
 # Streamlit app title
-st.title("GPT-3 Assistant")
+st.title("KOIOS czat")
+col1, col2  = st.columns([3,1])
 
 # Text input for prompt
 prompt = st.text_input("Prompt:", "")
+
+with col1:
+    st.write("Response:", "")
+    st.write("Full Response:", "")
+    st.write("Thread Trace:", "")
+
+with col2:
+    choice1 = st.radio("Wybierz model AI: ", ['GPT', 'Anton'])
 
 # Button to submit prompt
 if st.button("Submit"):
@@ -14,8 +23,8 @@ if st.button("Submit"):
         instructions = "you chat with me. if you find nothing in the files, search internet"
         assistant_type = "GPT3"
         response_ai, full_response, thread = query_model(prompt, instructions, assistant_type)
-
+        with col1:
+            st.write("Response:", response_ai)
+            st.write("Full Response:", full_response)
+            st.write("Thread Trace:", thread.id)
         # Display the response and thread ID
-        st.write("Response:", response_ai)
-        st.write("Full Response:", full_response)
-        st.write("Thread Trace:", thread.id)
