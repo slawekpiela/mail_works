d1798ccf44f8a61c05b95681648a82e7ec73d87c
slawek <slawek.piela@koios-mail.pl>
2024-01-07 23:58:59 +0100

    introducing sessions

--- web_chat.py
+++ web_chat.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 # Function to initialize session-specific data
 def initialize_session_data():
     if 'user_data' not in st.session_state:
@@ -9,26 +10,27 @@ def initialize_session_data():
             'prompt': None
         }
 
+
 # Initialize session data
+
 initialize_session_data()
 
+# draw the page
+
+st.title("KOIOS v0.1")
+st.write(f"Session ID: {st.session_state['session_id']}")
 col1, col2 = st.columns([3, 1])
 with col1:
     st.write("Response:", "")
-    #st.write("Full Response:", "")
+    # st.write("Full Response:", "")
     st.write("Thread Trace:", "")
 with col2:
     choice1 = st.radio("Wybierz model AI: ", ['GPT', 'Anton'])
-
-
-
 
 # Example of handling other session-specific data
 prompt = st.text_input("Prompt:")
 if st.button('Update Data'):
     st.session_state['user_data']['prompt'] = prompt
-
-
 
 if st.session_state['user_data']['prompt']:
     st.write(f"You entered: {st.session_state['user_data']['prompt']}")
