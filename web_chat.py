916b1c7f41db50e777433bba028cd3e54bdd6014
slawek <slawek.piela@koios-mail.pl>
2024-01-07 23:37:46 +0100

    updated for work with streamlit v14

--- web_chat.py
+++ web_chat.py
@@ -1,14 +1,20 @@
 import streamlit as st
 from query_openai import query_model
 import os
-
+import uuid
 
 assistant = str(os.getenv('assistant_id4'))
 
-
-
 # Streamlit app title
 st.title("KOIOS v0.1")
+
+# Initialize or retrieve the session ID
+if 'session_id' not in st.session_state:
+    st.session_state['session_id'] = str(uuid.uuid4())
+
+# Display the session ID
+st.write(f"Session ID: {st.session_state['session_id']}")
+
 col1, col2 = st.columns([3, 1])
 
 # Text input for prompt
@@ -32,4 +38,3 @@ if st.button("Submit"):
             st.write("Response:", response_ai)
             st.write("Full Response:", full_response)
             st.write("Thread Trace:", thread.id)
-
